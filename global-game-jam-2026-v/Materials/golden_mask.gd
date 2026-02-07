daa7b955b032f71a631b0eafdaee904e18ab484c
May Chlibi <meychlibi@gmail.com>
2026-02-07 02:07:31 +0100

    Collision MCHET

--- global-game-jam-2026-v/Materials/golden_mask.gd
+++ global-game-jam-2026-v/Materials/golden_mask.gd
@@ -1,25 +1,6 @@
 extends Area3D
 
 func _ready():
-<<<<<<< Updated upstream
-	# Connect the signal automatically
-	body_entered.connect(_on_body_entered)
-
-func _on_body_entered(body):
-	# 1. Check if the thing hitting the mask is a 'Character' (from your script)
-	if body is Character:
-		print("Collision detected with player!")
-		
-		# 2. Tell the player to show their head mask on all clients
-		body.equip_mask_visual.rpc()
-		
-		# 3. Add it to the inventory (using your existing function)
-		if multiplayer.is_server():
-			body.request_add_item("golden_mask", 1)
-		
-		# 4. Delete the floor mask so it's "picked up"
-		queue_free()
-=======
 	# Connect the signal so the function below runs on hit
 	body_entered.connect(_on_body_entered)
 
@@ -34,4 +15,3 @@ func _on_body_entered(body):
 			body.request_add_item.rpc_id(1, "golden_mask", 1) 
 		
 		queue_free() # Delete the mask from the world
->>>>>>> Stashed changes
