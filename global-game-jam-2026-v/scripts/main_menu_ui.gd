f1bace2f3ea71d7675b44d07fb1b14c192794602
mahdibennsir <mahdibennsir123@gmail.com>
2026-02-08 11:50:48 +0100

    ui
    haja s8ira

--- global-game-jam-2026-v/scripts/main_menu_ui.gd
+++ global-game-jam-2026-v/scripts/main_menu_ui.gd
@@ -5,21 +5,22 @@ signal host_pressed(nickname: String, skin: String)
 signal join_pressed(nickname: String, skin: String, address: String)
 signal quit_pressed
 
-@onready var skin_input: LineEdit = $MainContainer/MainMenu/Option2/SkinInput
 @onready var nick_input: LineEdit = $MainContainer/MainMenu/Option1/NickInput
 @onready var address_input: LineEdit = $MainContainer/MainMenu/Option3/AddressInput
-
+@onready var option_button: OptionButton = $MainContainer/MainMenu/Option2/OptionButton
 func _ready():
 	pass
 
 func _on_host_pressed():
 	var nickname = nick_input.text.strip_edges()
-	var skin = skin_input.text.strip_edges().to_lower()
+	var selected_idx = option_button.get_selected_id()
+	var skin = option_button.get_item_text(selected_idx).to_lower()
 	host_pressed.emit(nickname, skin)
 
 func _on_join_pressed():
 	var nickname = nick_input.text.strip_edges()
-	var skin = skin_input.text.strip_edges().to_lower()
+	var selected_idx = option_button.get_selected_id()
+	var skin = option_button.get_item_text(selected_idx).to_lower()
 	var address = address_input.text.strip_edges()
 	join_pressed.emit(nickname, skin, address)
 
@@ -39,7 +40,11 @@ func get_nickname() -> String:
 	return nick_input.text.strip_edges()
 
 func get_skin() -> String:
-	return skin_input.text.strip_edges().to_lower()
+	# Get the index of the currently selected item
+	var selected_idx = option_button.get_selected_id()
+	
+	# Return the text of that item, formatted as lowercase
+	return option_button.get_item_text(selected_idx).to_lower()
 
 func get_address() -> String:
 	return address_input.text.strip_edges()
